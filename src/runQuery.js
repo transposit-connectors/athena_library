a3eafb1c6f73fce897cbee3de0a6aa3d4ee3677c
Dan Moore <mooreds@transposit.com>
2019-10-03 21:39:33 +0000

    initial checkin

---
+++ src/runQuery.js
@@ -0,0 +1,13 @@
+(params) => {
+  let executionId = api.run("aws_athena.start_query_execution", {
+    $body: {
+      QueryString: params.query,
+      ClientRequestToken: api.run("this.makeId")[0],
+      ResultConfiguration: {
+        OutputLocation: params.resultlocation
+      }
+    }
+  })[0]["QueryExecutionId"];
+  
+  stash.put("query-id", executionId);
+}
